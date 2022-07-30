b11fbfa7ab3a7e6d0d6c9589b64b8e5802e4acd3
Banyc <36535895+Banyc@users.noreply.github.com>
2022-07-30 18:02:27 +0800

    feat: save optimal values and actions

--- src/bin/jacks_car_rental.rs
+++ src/bin/jacks_car_rental.rs
@@ -1,6 +1,14 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::Write,
+    path,
+};
 
 use value_iteration::{jacks_car_rental::JacksCarRental, value_iteration::ValueIteration};
+
+const VALUE_OUTPUT_FILE: &str = "jacks_car_rental.value.csv";
+const ACTION_OUTPUT_FILE: &str = "jacks_car_rental.action.csv";
 
 fn main() {
     let task = JacksCarRental;
@@ -10,13 +18,32 @@ fn main() {
         v.insert(s, 0.0);
     }
     value_iteration.value_iteration(10.0, &mut v);
-    println!("(s, V(s))");
-    for s in value_iteration.task().state_space() {
-        println!("({:?}, {})", s, v[&s]);
+    {
+        println!("(s, V(s))");
+        if let Err(e) = fs::remove_file(path::Path::new(VALUE_OUTPUT_FILE)) {
+            if e.kind() != std::io::ErrorKind::NotFound {
+                panic!("{}", e);
+            }
+        }
+        let mut file = File::create(VALUE_OUTPUT_FILE).unwrap();
+        for s in value_iteration.task().state_space() {
+            println!("({:?}, {})", s, v[&s]);
+            writeln!(file, "{}, {}, {}", s.0, s.1, v[&s]).unwrap();
+        }
     }
     println!();
-    println!("(s, a)");
-    for s in value_iteration.task().state_space() {
-        println!("({:?}, {:?})", s, value_iteration.max_v_a(&v, &s).1);
+    {
+        println!("(s, a)");
+        if let Err(e) = fs::remove_file(path::Path::new(ACTION_OUTPUT_FILE)) {
+            if e.kind() != std::io::ErrorKind::NotFound {
+                panic!("{}", e);
+            }
+        }
+        let mut file = File::create(ACTION_OUTPUT_FILE).unwrap();
+        for s in value_iteration.task().state_space() {
+            let a = value_iteration.max_v_a(&v, &s).1;
+            println!("({:?}, {:?})", s, a);
+            writeln!(file, "{}, {}, {:?}", s.0, s.1, a).unwrap();
+        }
     }
 }
