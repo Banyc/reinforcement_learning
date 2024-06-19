8a16adb8f554d06815ef542f5b9136b46f6df12b
Banyc <36535895+Banyc@users.noreply.github.com>
2024-06-19 23:37:12 +0800

    docs: monte carlo

--- src/monte_carlo.rs
+++ src/monte_carlo.rs
@@ -25,89 +25,105 @@ where
         Self { task }
     }
 
+    /// - `value`: $Q$
+    /// - `weight_sum`: $C$
+    /// - `policy`: $\pi$
+    /// - `prob_explore`: $\epsilon$
     pub fn policy_evaluation(
         &self,
-        q: &mut HashMap<StateActionPair<State, Action>, f64>,
-        c: &mut HashMap<StateActionPair<State, Action>, f64>,
-        pi: &mut HashMap<State, Vec<Action>>,
-        e: f64,
+        value: &mut HashMap<StateActionPair<State, Action>, f64>,
+        l_importance_sum: &mut HashMap<StateActionPair<State, Action>, f64>,
+        policy: &mut HashMap<State, Vec<Action>>,
+        prob_explore: f64,
         num_episodes: usize,
     ) {
         for _ in 0..num_episodes {
-            // generate an episode
-            // using epsilon-greedy policy b
-            let mut episode = vec![];
-            {
-                let mut s = self.task.random_state();
-                while !self.task.in_terminal_state_space(&s) {
-                    let mut rng = rand::thread_rng();
-                    let rnd = rng.gen_range(0.0..1.0);
-                    // epsilon-greedy policy
-                    let a = if rnd < e {
-                        self.task.random_action(&s)
-                    } else {
-                        match pi.get(&s) {
-                            Some(a) => *a.choose(&mut rng).unwrap(),
-                            None => self.task.random_action(&s),
-                        }
-                    };
-                    let (s_next, r) = self.task.transit(&s, &a);
-                    episode.push(Step {
-                        state: s,
-                        action: a,
-                        reward: r,
-                    });
-                    s = s_next;
-                }
-            }
+            let episode = self.generate_episode(policy, prob_explore);
 
-            let mut g = 0.0;
-            let mut w = 1.0;
+            let mut step_ret = 0.0;
+            let mut learning_importance = 1.0;
             for step in episode.iter().rev() {
-                g = self.task.gamma() * g + step.reward;
-                let s_a = StateActionPair {
+                step_ret = self.task.gamma() * step_ret + step.reward;
+                let state_then_action = StateActionPair {
                     state: step.state,
                     action: step.action,
                 };
+                let learning_rate = {
+                    // learning rate decays in an $1/n$ manner on $n$-th step
+                    let l_importance_sum = l_importance_sum.entry(state_then_action).or_insert(0.);
+                    *l_importance_sum += learning_importance;
+                    learning_importance / *l_importance_sum
+                };
                 {
-                    let c_old = c.get(&s_a).unwrap_or(&0.0);
-                    c.insert(s_a, c_old + w);
+                    // Nudge value towards the step return for this action on the current state
+                    let v = value.get(&state_then_action).unwrap_or(&0.0);
+                    let shift = learning_rate * (step_ret - v);
+                    value.insert(state_then_action, v + shift);
                 }
-                {
-                    let q_old = q.get(&s_a).unwrap_or(&0.0);
-                    let shift = w / c.get(&s_a).unwrap() * (g - q_old);
-                    q.insert(s_a, q_old + shift);
-                }
-                {
-                    let (_, a) = self.max_q_a(q, &step.state);
-                    pi.insert(step.state, a);
-                }
-                if !pi.get(&step.state).unwrap().contains(&step.action) {
+                let best_actions = {
+                    // Set the best actions to the policy
+                    let (_, a) = self.max_value_by_actions(value, &step.state);
+                    policy.insert(step.state, a);
+                    policy.get(&step.state).unwrap()
+                };
+                if !best_actions.contains(&step.action) {
+                    // Early terminate the episode that goes on the suboptimal direction
                     break;
                 }
                 {
-                    let pi_a_len = pi.get(&step.state).unwrap().len();
-                    // probability of the action under the epsilon-greedy policy b
-                    let b_p_a = {
-                        (1.0 - e) / pi_a_len as f64
-                            + e / self.task.action_space_len(&step.state) as f64
+                    // Adjust learning rate
+                    let prob_explored_best_actions = {
+                        let num_best = best_actions.len();
+                        let num_all = self.task.action_space_len(&step.state);
+                        prob_explore * num_best as f64 / num_all as f64
                     };
-
-                    w *= 1.0 / (b_p_a * pi_a_len as f64);
+                    // Learn more from the immediate past with a correctly explored future
+                    learning_importance *= 1. / prob_explored_best_actions;
                 }
             }
         }
     }
 
-    pub fn max_q_a(
+    fn generate_episode(
         &self,
-        q: &HashMap<StateActionPair<State, Action>, f64>,
+        policy: &HashMap<State, Vec<Action>>,
+        prob_explore: f64,
+    ) -> Vec<Step<State, Action>> {
+        // using epsilon-greedy policy b
+        let mut episode = vec![];
+        let mut s = self.task.random_state();
+        while !self.task.in_terminal_state_space(&s) {
+            let mut rng = rand::thread_rng();
+            let rnd = rng.gen_range(0.0..1.0);
+            // epsilon-greedy policy
+            let a = if rnd < prob_explore {
+                self.task.random_action(&s)
+            } else {
+                match policy.get(&s) {
+                    Some(a) => *a.choose(&mut rng).unwrap(),
+                    None => self.task.random_action(&s),
+                }
+            };
+            let (s_next, r) = self.task.transit(&s, &a);
+            episode.push(Step {
+                state: s,
+                action: a,
+                reward: r,
+            });
+            s = s_next;
+        }
+        episode
+    }
+
+    pub fn max_value_by_actions(
+        &self,
+        value: &HashMap<StateActionPair<State, Action>, f64>,
         s: &State,
     ) -> (f64, Vec<Action>) {
         let mut max_v = f64::MIN;
         let mut max_a = vec![];
         for a in self.task.action_space(s) {
-            let v = *q
+            let v = *value
                 .get(&StateActionPair {
                     state: *s,
                     action: a,
